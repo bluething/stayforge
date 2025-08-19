113aa1ec07a21dbdfa177ded3f9ca8dfc6702f41
Habib Ali Machpud <habib.machpud@gmail.com>
2025-08-19 23:39:20 +0700

    Init

---
+++ supply-api/src/main/java/io/github/bluething/stayforge/supplyapi/SupplyApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.stayforge.supplyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupplyApiApplication.class, args);
+    }
+
+}
---
+++ supply-api/src/test/java/io/github/bluething/stayforge/supplyapi/SupplyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.stayforge.supplyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
